251f860bd5489e4c63be660fcc8f5bf77c18ae87
genestera <genesistera@mediacat.ne.jp>
2018-11-09 09:57:48 +0900

    version 1.00.01
    update18.11.9

--- speech_text2.py
+++ speech_text2.py
@@ -1,15 +1,21 @@
 from watson_developer_cloud import SpeechToTextV1
+import sys,os.path
 import json
 
-def text_json(out_f="out1.wav",lang_k="ja-JP_BroadbandModel"):
+def text_json(out_f="out1.mp3",lang_k="ja-JP_BroadbandModel"):
     user='ユーザー名'
     pswd='パスワード'
     audio_file = open(out_f, "rb")
-    cont_type = "audio/wav"
+    ext = os.path.splitext(out_f)[1][1:]
+    cont_type = "audio/" + ext
+    print (cont_type)
     lang = lang_k
     # ワトソンとの送信と受信
     stt = SpeechToTextV1(username=user, password=pswd)
-    result_json = stt.recognize(audio=audio_file, content_type=cont_type, model=lang)
+    result_json = stt.recognize(
+            audio=audio_file,
+            content_type=cont_type,
+            model=lang).get_result()
     # ファイルの保存
     with open("result.json","w") as f:
         json.dump(result_json,f,ensure_ascii=False,indent=2)
